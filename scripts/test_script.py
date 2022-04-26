9611ef1e59804cae30d4f075743e41ae12712968
hiro <hirofeynman@gmail.com>
2022-04-26 15:55:41 +0200

    test scripts

--- scripts/test_script.py
+++ scripts/test_script.py
@@ -3,10 +3,10 @@ from pathlib import Path
 from processing.preprocessing import process_single_subject
 
 if __name__ == "__main__":
-    src_dir = "/data/home/hiroyoshi/test-src"
-    dst_dir = "/data/home/hiroyoshi/test-dst"
-    events_dir = "/data/home/hiroyoshi/events_dir"
-    param_dir = "/data/home/hiroyoshi/param_dir"
+    src_dir = "/data/home/hiroyoshi/test-dir/test-src"
+    dst_dir = "/data/home/hiroyoshi/test-dir/test-dst"
+    events_dir = "/data/home/hiroyoshi/test-dir/events_dir"
+    param_dir = "/data/home/hiroyoshi/test-dir/param_dir"
     subject_name = "sub-V1010"
 
     params = read_json(Path(param_dir), "preprocess_params.json")

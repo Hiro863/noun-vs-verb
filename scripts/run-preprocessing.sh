22673775c6b6a0bac914c16d79bfd3e74a15bb03
hiro <hirofeynman@gmail.com>
2022-04-28 20:06:01 +0200

    test scripts

--- scripts/run-preprocessing.sh
+++ scripts/run-preprocessing.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-#SBATCH --array=1,2,3
-#SBATCH --ntasks-per-node=10
-
 #SBATCH --job-name=test_preprocessing
+#SBATCH --array=1,2,3          ## subject IDs
+#SBATCH --ntasks-per-node=10   ## number of cores per subject
+
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=hiroyoshi.yamasaki@etu.univ-amu.fr
 
 #SBATCH --chdir=/data/home/hiroyoshi/logs
 
 #SBATCH --mem=15gb
-#SBATCH --ntasks=10
 
-echo SLURM_ARRAY_TASK_ID
-echo SLURM_NTASKS_PER_NODE
-echo "--"
+
 export PYTHONPATH=$PYTHONPATH:/data/home/hiroyoshi/scripts/meg-mvpa
-python /data/home/hiroyoshi/scripts/meg-mvpa/scripts/test_preprocessing.py $SLURM_ARRAY_TASK_ID $SLURM_NTASKS_PER_NODE
+python scripts/test_preprocessing.py $SLURM_ARRAY_TASK_ID $SLURM_NTASKS_PER_NODE

450ffa845c03b1bd9fce663db04a4584deb314bc
hiro <hirofeynman@gmail.com>
2022-04-29 16:52:13 +0200

    freesurfer

--- scripts/freesurfer-recon.sh
+++ scripts/freesurfer-recon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --array=4,5,6
+#SBATCH --array=7,8,9,10,11,12,13,14,15,16,17,18,19,20
 #SBATCH --ntasks-per-node=10
 #SBATCH --job-name=freesurfer-recon-all
 #SBATCH --mail-type=BEGIN,END,

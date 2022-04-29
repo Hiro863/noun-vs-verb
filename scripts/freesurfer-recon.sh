9d7487658195036e9ffd901e9b07f68b07c01a35
hiro <hirofeynman@gmail.com>
2022-04-29 15:28:54 +0200

    freesurfer

--- scripts/freesurfer-recon.sh
+++ scripts/freesurfer-recon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --array=sub-V1001,sub-V1003,sub-V1004          ## subject IDs
+#SBATCH --array=1001,1003,1004          ## subject IDs
 #SBATCH --ntasks-per-node=10
 #SBATCH --job-name=freesurfer-recon-all
 #SBATCH --mail-type=BEGIN,END,
@@ -12,8 +12,8 @@ export FREESURFER_HOME=/data/home/hiroyoshi/freesurfer
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 export SUBJECTS_DIR=/data/home/hiroyoshi/freesurfer/subjects
 
-my_subject="${SLURM_ARRAY_TASK_ID}"
-my_NIfTI="/data/home/hiroyoshi/data/MOUS/${SLURM_ARRAY_TASK_ID}/anat/${SLURM_ARRAY_TASK_ID}_T1w.nii"
+my_subject="sub-V${SLURM_ARRAY_TASK_ID}"
+my_NIfTI="/data/home/hiroyoshi/data/MOUS/sub-V${SLURM_ARRAY_TASK_ID}/anat/sub-V${SLURM_ARRAY_TASK_ID}_T1w.nii"
 srun recon-all -i $my_NIfTI -s $my_subject -all
 
 

5f30a03a63e88adcc3c1bb0fcf3cc6a5d22e0cc5
hiro <hirofeynman@gmail.com>
2022-05-03 13:57:47 +0200

    coreg

--- scripts/run_coregistration.py
+++ scripts/run_coregistration.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     dst_dir = data_dir / subj_name
 
     # Read raw
-    raw = read_mous_subject(data_dir / subj_name)
-    info = read_info(str(data_dir / subj_name))
+    #raw = read_mous_subject(data_dir / subj_name)
+    info = read_info("/data/home/hiroyoshi/data/MOUS/sub-V1001/meg/sub-V1001_task-visual_meg.ds")#str(data_dir / subj_name))
 
     get_trans(subject=str(subjects_dir), dst_dir=dst_dir, subjects_dir=subjects_dir, info=info)

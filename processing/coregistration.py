2659a7d3c3945172f6284c890872e9790f00cbf0
hiro <hirofeynman@gmail.com>
2022-05-03 13:13:32 +0200

    coreg

--- processing/coregistration.py
+++ processing/coregistration.py
@@ -4,8 +4,10 @@ from mne import Info, write_trans
 
 
 def get_trans(subject: str, dst_dir: Path, subjects_dir: Path, info: Info):
-    coreg = Coregistration(info, subject, subjects_dir, fiducials="estimated")
+    coreg = Coregistration(info, subject=subject, subjects_dir=subjects_dir, fiducials="estimated")
     coreg.fit_icp(n_iterations=6, nasion_weight=2.)
     coreg.omit_head_shape_points(distance=5. / 1000)
     coreg.fit_icp(n_iterations=20, nasion_weight=10.)
     write_trans(str(dst_dir / f"{subject}-trans.fif"), coreg.trans)
+
+#def get_forward(subject: str, subjects_dir: Path):
--- scripts/freesurfer-bem.sh
+++ scripts/freesurfer-bem.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --array=110,111,113,114,115,116,117
+#SBATCH --array=1            ## subject ID
 #SBATCH --ntasks-per-node=1
-#SBATCH --job-name=freesurfer-recon-all
+#SBATCH --job-name=freesurfer-bem
 #SBATCH --mail-type=BEGIN,END,
 #SBATCH --mail-user=hiroyoshi.yamasaki@etu.univ-amu.fr
 #SBATCH --chdir=/data/home/hiroyoshi/logs
@@ -12,8 +12,8 @@ export FREESURFER_HOME=/data/home/hiroyoshi/freesurfer
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 export SUBJECTS_DIR=/data/home/hiroyoshi/freesurfer/subjects
 
-my_subject="sub-V10${SLURM_ARRAY_TASK_ID}"
-my_NIfTI="/data/home/hiroyoshi/data/MOUS/sub-V1${SLURM_ARRAY_TASK_ID}/anat/sub-V1${SLURM_ARRAY_TASK_ID}_T1w.nii"
-recon-all -i $my_NIfTI -s $my_subject -all
+my_subject="sub-V1%03d${SLURM_ARRAY_TASK_ID}"
+mne watershed_bem -s sample
+
 
 

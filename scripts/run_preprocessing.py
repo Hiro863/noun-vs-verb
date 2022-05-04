a43fa0ed8ab88c31643fca9d556784b27f7e8bc9
hiro <hirofeynman@gmail.com>
2022-05-04 11:26:08 +0200

    preprocessing

--- scripts/run_preprocessing.py
+++ scripts/run_preprocessing.py
@@ -8,18 +8,20 @@ if __name__ == "__main__":
     # Get input from the bash script
     subj_id = sys.argv[1]
     n_cores = int(sys.argv[2])
+    param_dir = Path(sys.argv[3])
 
     subj_name = f"sub-V1{str(subj_id).zfill(3)}"
 
-    # Set up directories
-    root = Path("/data/home/hiroyoshi/test-dir")
-    src_dir = Path("/data/home/hiroyoshi/data/MOUS") / subj_name
-    dst_dir = root / "test-dst"
-    events_dir = root / "events_dir"
-    param_dir = root / "param_dir"
-
+    # Get the parameters
     params = read_json(param_dir, "preprocess_params.json")
-    process_single_subject(src_dir, dst_dir, events_dir, subj_name,
+
+    # Set up directories
+    root = Path(params["directories"]["root"])
+    raw_dir = Path(params["directories"]["raw-dir"]) / subj_name
+    epochs_dir = root / "epochs-dir"
+    events_dir = root / "events-dir"
+
+    process_single_subject(raw_dir, epochs_dir, events_dir, subj_name,
                            downsample_params=params["downsample params"], filter_params=params["filter params"],
                            artifact_params=params["artifact params"], epoch_params=params["epoch params"],
                            stc_params=params["stc params"],

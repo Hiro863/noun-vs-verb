20bfdd4ac74302077fd2dbd481a917fb2edc36e2
hiro <hirofeynman@gmail.com>
2022-05-05 16:53:52 +0200

    fwd

--- scripts/run-make-fwds.sh
+++ scripts/run-make-fwds.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=preprocessing
-#SBATCH --array=20
+#SBATCH --array=22,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,42,44,45,46,48,49,50,52,53,54,55,57,58,59
 #SBATCH --ntasks-per-node=1   ## number of cores per subject
 
 #SBATCH --mail-type=BEGIN,END

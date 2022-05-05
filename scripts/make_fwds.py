43a0c5c831294672352c485c1d00669ba86a2147
hiro <hirofeynman@gmail.com>
2022-05-05 15:48:48 +0200

    preprocessing

--- scripts/make_fwds.py
+++ scripts/make_fwds.py
@@ -6,41 +6,35 @@ from utils.file_access import get_mous_raw_paths, read_mous_subject
 from processing.coregistration import get_trans, get_forward
 
 
-def main(raw_dir, subjects_dir, fwd_dir):
+def main(raw_dir, subjects_dir, fwd_dir, subject):
 
     raw_paths = get_mous_raw_paths(raw_dir)
 
-    failed_list = []
-    for path, subject in raw_paths:
-        if subject in ["sub-V1001", "sub-V1002", "sub-V1003", "sub-V1006"]:
-            continue
-        if re.match(r"^sub-A\d+", subject):  # e.g. sub-A2002
-            continue
-        print(subjects_dir)
-        raw = read_mous_subject(path, preload=False)
+    path = raw_dir / subject / "meg" / "sub-V1020_task-visual_meg.ds"
+    raw = read_mous_subject(path, preload=False)
 
-        trans = get_trans(subject=subject, subjects_dir=subjects_dir, info=raw.info)
-        """try:
-            fwd = get_forward(info=raw.info, trans=trans, subject=subject, subjects_dir=subjects_dir, layers=3)
-            write_forward_solution(fwd_dir / f"{subject}-fwd.fif", fwd, overwrite=True)
-        except RuntimeError as e:
-            print(f"Runtime error: {e}")
-            print(f"Failed to make forward model with three layers for the subject {subject}")
-            print("Trying one layer instead...")
-            failed_list.append(f"{subject}: failed for 3 layers")"""
+    trans = get_trans(subject=subject, subjects_dir=subjects_dir, info=raw.info)
+    """try:
+        fwd = get_forward(info=raw.info, trans=trans, subject=subject, subjects_dir=subjects_dir, layers=3)
+        write_forward_solution(fwd_dir / f"{subject}-fwd.fif", fwd, overwrite=True)
+    except RuntimeError as e:
+        print(f"Runtime error: {e}")
+        print(f"Failed to make forward model with three layers for the subject {subject}")
+        print("Trying one layer instead...")
+        failed_list.append(f"{subject}: failed for 3 layers")"""
 
-        try:
-            fwd = get_forward(info=raw.info, trans=trans, subject=subject, subjects_dir=subjects_dir, layers=1)
-            write_forward_solution(fwd_dir / f"{subject}-fwd.fif", fwd, overwrite=True)
-        except RuntimeError as e:
-            print(f"Runtime error: {e}")
-            print(f"Failed to make forward model with one layers")
-            failed_list.append(f"{subject}: failed for 1 layer")
+    try:
+        fwd = get_forward(info=raw.info, trans=trans, subject=subject, subjects_dir=subjects_dir, layers=1)
+        write_forward_solution(fwd_dir / f"{subject}-fwd.fif", fwd, overwrite=True)
+    except RuntimeError as e:
+        print(f"Runtime error: {e}")
+        print(f"Failed to make forward model with one layers")
 
 
 if __name__ == "__main__":
     mous_dir = Path(sys.argv[1])
     subjects_dir = sys.argv[2]
     fwd_dir = Path(sys.argv[3])
+    subj_id = int(sys.argv[4])
 
-    main(mous_dir, subjects_dir, fwd_dir)
+    main(mous_dir, subjects_dir, fwd_dir, f"sub-V10{subj_id}")
---
+++ scripts/run-make-fwds.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=preprocessing
+#SBATCH --array=20
+#SBATCH --ntasks-per-node=1   ## number of cores per subject
+
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=hiroyoshi.yamasaki@etu.univ-amu.fr
+
+#SBATCH --chdir=/data/home/hiroyoshi/logs
+
+#SBATCH --mem=10gb
+
+scripts_dir=/data/home/hiroyoshi/scripts/meg-mvpa/scripts  # scripts directory
+mous_dir=/data/home/hiroyoshi/data/MOUS
+subjects_dir=/data/home/hiroyoshi/freesurfer/subjects
+fwd_dir=/data/home/hiroyoshi/MOUS/fwds
+
+export PYTHONPATH=$PYTHONPATH:/data/home/hiroyoshi/scripts/meg-mvpa
+
+python $scripts_dir/make_fwds.py $mous_dir $subjects_dir $fwd_dir $SLURM_ARRAY_TASK_ID

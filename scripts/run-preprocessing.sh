cf28dcdff32e949ecd6995a7dfecf797e6fcf802
hiro <hirofeynman@gmail.com>
2022-05-05 17:45:12 +0200

    preprocessing

--- scripts/run-preprocessing.sh
+++ scripts/run-preprocessing.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=preprocessing
-#SBATCH --array=1,2,3,4,5,7,8
+#SBATCH --array=9,10,11,20,22,24,25,26,27,28,29,30,31,32,34,35,36,37,38,39
 #SBATCH --ntasks-per-node=10   ## number of cores per subject
 
 #SBATCH --mail-type=BEGIN,END

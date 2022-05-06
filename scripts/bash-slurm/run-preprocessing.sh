ea78918dd7f472ee6e1fdcc67fdf4db05503ed35
hiro <hirofeynman@gmail.com>
2022-05-06 09:54:36 +0200

    preprocessing

--- scripts/bash-slurm/run-preprocessing.sh
+++ scripts/bash-slurm/run-preprocessing.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --job-name=preprocessing
-#SBATCH --array=1-117          ## subject IDs
+#SBATCH --array=1-2 ##117          ## subject IDs
 #SBATCH --ntasks-per-node=10   ## number of cores per subject
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=hiroyoshi.yamasaki@etu.univ-amu.fr
 #SBATCH --chdir=/data/home/hiroyoshi/logs
-#SBATCH --mem=50gb   ## filtering and ICA requires memory
+#SBATCH --mem=30gb   ## filtering and ICA requires memory
 
 # Skip non-existent subjects
 # no_subject=(14 18 21 23 41 43 47 51 56 60 67 82 91 112)  # non-existent subjects

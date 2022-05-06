7544e94e0cea1158a700f7d600caea1f30cd87e4
hiro <hirofeynman@gmail.com>
2022-05-06 09:41:42 +0200

    preprocessing

--- scripts/bash-slurm/run-preprocessing.sh
+++ scripts/bash-slurm/run-preprocessing.sh
@@ -6,15 +6,13 @@
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=hiroyoshi.yamasaki@etu.univ-amu.fr
 #SBATCH --chdir=/data/home/hiroyoshi/logs
-#SBATCH --mem=100gb   ## filtering and ICA requires memory
+#SBATCH --mem=50gb   ## filtering and ICA requires memory
 
 # Skip non-existent subjects
 no_subject=(14 18 21 23 41 43 47 51 56 60 67 82 91 112)  # non-existent subjects
 if [[ ! " ${no_subject[*]} " =~ ${SLURM_ARRAY_TASK_ID} ]]; then
   echo "Subject ${SLURM_ARRAY_TASK_ID} does not exist"
   exit 0
-else
-  echo $SLURM_ARRAY_TASK_ID
 fi
 
 scripts_dir=/data/home/hiroyoshi/scripts/meg-mvpa/scripts/py_slurm  # scripts directory

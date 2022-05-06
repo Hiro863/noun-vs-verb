325c34c7c292aa0bfbebe476e8b052982306ab48
hiro <hirofeynman@gmail.com>
2022-05-06 09:25:23 +0200

    preprocessing

--- scripts/bash-slurm/run-preprocessing.sh
+++ scripts/bash-slurm/run-preprocessing.sh
@@ -11,7 +11,7 @@
 # Skip non-existent subjects
 no_subject=(14 18 21 23 41 43 47 51 56 60 67 82 91 112)  # non-existent subjects
 if [[ " ${no_subject[*]} " =~ ${SLURM_ARRAY_TASK_ID} ]]; then
-  echo "This subject does not exist"
+  echo "Subject ${SLURM_ARRAY_TASK_ID} does not exist"
   exit 0
 fi
 
@@ -19,4 +19,4 @@ scripts_dir=/data/home/hiroyoshi/scripts/meg-mvpa/scripts/py_slurm  # scripts di
 param_dir=/data/home/hiroyoshi/results/param-dir                    # parameter directory
 
 export PYTHONPATH=$PYTHONPATH:/data/home/hiroyoshi/scripts/meg-mvpa
-python $scripts_dir/run_preprocessing.py $"SLURM_ARRAY_TASK_ID" $"SLURM_NTASKS_PER_NODE" $param_dir
+python $scripts_dir/run_preprocessing.py $SLURM_ARRAY_TASK_ID $SLURM_NTASKS_PER_NODE $param_dir

9e04991d6f89ef438021ccf4ddf26b91aeac16e0
hiro <hirofeynman@gmail.com>
2022-05-06 18:02:50 +0200

    source

--- scripts/bash-slurm/run-source-localization.sh
+++ scripts/bash-slurm/run-source-localization.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=source-localization
-#SBATCH --array=1  #117           ## subject IDs
+#SBATCH --array=1-117           ## subject IDs
 #SBATCH --ntasks-per-node=1         ## number of cores per subject
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=hiroyoshi.yamasaki@etu.univ-amu.fr

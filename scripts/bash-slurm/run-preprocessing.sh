2869bec1ab06e0092114dc88d96ec39372332806
hiro <hirofeynman@gmail.com>
2022-05-06 09:54:53 +0200

    preprocessing

--- scripts/bash-slurm/run-preprocessing.sh
+++ scripts/bash-slurm/run-preprocessing.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=preprocessing
 #SBATCH --array=1-2 ##117          ## subject IDs
-#SBATCH --ntasks-per-node=10   ## number of cores per subject
+#SBATCH --ntasks-per-node=5   ## number of cores per subject
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=hiroyoshi.yamasaki@etu.univ-amu.fr
 #SBATCH --chdir=/data/home/hiroyoshi/logs

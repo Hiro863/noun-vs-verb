33f027ef0ddbff6aa76de764e2ba101a413f8e76
hiro <hirofeynman@gmail.com>
2022-05-09 16:15:58 +0200

    preprocess

--- scripts/bash-slurm/run-preprocessing.sh
+++ scripts/bash-slurm/run-preprocessing.sh
@@ -11,12 +11,13 @@
 # Skip non-existent subjects
 
 no_subject=(14 15 "18" 21 23 41 43 47 51 56 60 67 82 91 112)  # non-existent subjects
-if [[ "${no_subject[0]}" =~ ^${SLURM_ARRAY_TASK_ID}$ ]]; then
-  echo "Subject ${SLURM_ARRAY_TASK_ID} does not exist"
-  exit 0
-else
-  echo "something wrong"
-fi
+for i in "${no_subject[@]}"
+do
+  if [[ "$i" =~ ^${SLURM_ARRAY_TASK_ID}$ ]]; then
+    echo "Subject ${SLURM_ARRAY_TASK_ID} does not exist"
+    exit 0
+  fi
+done
 
 # Skip problematic subjects
 prob_subjects=(6 9 10 14 33 44 90 97 97 107 109)  # some technical issues with these subjects

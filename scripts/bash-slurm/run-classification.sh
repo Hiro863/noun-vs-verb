9bae435fc3a6b72ddded56a8679a2d503bbec9b3
hiro <hirofeynman@gmail.com>
2022-05-10 14:30:23 +0200

    preprocess

--- scripts/bash-slurm/run-classification.sh
+++ scripts/bash-slurm/run-classification.sh
@@ -8,8 +8,8 @@
 #SBATCH --chdir=/data/home/hiroyoshi/logs
 #SBATCH --mem-per-cpu=10gb
 
-scripts_dir=/data/home/hiroyoshi/scripts/meg-mvpa/scripts  # scripts directory
-param_dir=/data/home/hiroyoshi/results/param-dir           # parameter directory
+scripts_dir=/data/home/hiroyoshi/scripts/meg-mvpa/scripts/py_slurm  # scripts directory
+param_dir=/data/home/hiroyoshi/results/param-dir                    # parameter directory
 
-export PYTHONPATH=$PYTHONPATH:/data/home/hiroyoshi/scripts/meg-mvpa/py_slurm
+export PYTHONPATH=$PYTHONPATH:/data/home/hiroyoshi/scripts/meg-mvpa
 python $scripts_dir/run_classification.py $SLURM_ARRAY_TASK_ID $SLURM_NTASKS_PER_NODE $param_dir

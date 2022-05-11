e1245f6fb35b99380889d11af0c3cdd0dba7d4b6
hiro <hirofeynman@gmail.com>
2022-05-11 11:19:57 +0200

    preprocess

--- scripts/py_slurm/run_classification.py
+++ scripts/py_slurm/run_classification.py
@@ -121,7 +121,7 @@ if __name__ == "__main__":
     # Get the parameters
     params = read_json(param_dir, "classification-params.json")
     parcellation = params["parcellation"]
-    path = Path(params["v"])
+    path = Path(params["idx-to-name"])
 # print(params)
     with open(path, "rb") as handle:
         idx_to_name = pickle.load(handle)

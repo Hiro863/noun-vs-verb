bf8f75e906f392a066f3a19b9de260e2f6dd44b2
hiro <hirofeynman@gmail.com>
2022-05-11 10:58:27 +0200

    preprocess

--- scripts/py_slurm/run_conditions.py
+++ scripts/py_slurm/run_conditions.py
@@ -29,12 +29,12 @@ if __name__ == "__main__":
     params = read_json(param_dir, "conditions-params.json")
 
     # Set up directories
-    root = Path(params["directories"]["root"])
+    root = Path(params["root"])
     dataset_dir = root / "dataset-dir"
 
-    with open(params["directories"]["idx-to-name"], "rb") as handle:
+    with open(params["idx-to-name"], "rb") as handle:
         idx_to_name = pickle.load(handle)
-    subjects_dir = params["directories"]["subjects-dir"]
+    subjects_dir = params["subjects-dir"]
     labels = read_labels_from_annot("fsaverage", params["parcellation"], params["hemi"], subjects_dir=subjects_dir)
     name = idx_to_name[area_id]
 

4d6a90f9526775e48b34467695e6c81819eaa373
hiro <hirofeynman@gmail.com>
2022-05-11 18:05:04 +0200

    preprocess

---
+++ scripts/run_format_event_data.py
@@ -0,0 +1,18 @@
+import os
+import re
+from pathlib import Path
+
+from events.formatting import format_event_data
+
+EVENTS_DIR = Path("")
+
+
+def main():
+
+    for path in os.listdir(EVENTS_DIR):
+        if re.mat
+
+
+
+if __name__ == "__main__":
+    main()

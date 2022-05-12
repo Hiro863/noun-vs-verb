6cecf975f148094482a02f5f5841c08af10320ee
hiro <hirofeynman@gmail.com>
2022-05-12 14:19:20 +0200

    preprocess

--- scripts/py_slurm/run_preprocessing.py
+++ scripts/py_slurm/run_preprocessing.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     root = Path(params["directories"]["root"])
     raw_dir = Path(params["directories"]["raw-dir"]) / subj_name
     epochs_dir = root / "epochs-dir" / subj_name
-    events_dir = root / "events-dir-"
+    events_dir = root / "events-dir"
 
     if not epochs_dir.exists():
         os.makedirs(epochs_dir)

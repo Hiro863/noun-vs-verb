2b3754be3a9f867a0d58b5c6a8d6060987951acc
hiro <hirofeynman@gmail.com>
2022-05-13 09:37:51 +0200

    preprocess

--- processing/dataset.py
+++ processing/dataset.py
@@ -97,9 +97,9 @@ def _validate_paths(stc_paths, events_paths):
     valid_stcs, valid_events = [], []
     for stc_path in stc_paths:
         subject = re.findall(r"sub-[AV]\d+", str(stc_path))[0]
-
+        print(subject)
         for event_path in events_paths:
-            if re.match(subject, str(event_path)):
+            if re.match(rf"{subject}", str(event_path)):
 
                 valid_stcs.append(stc_path)
                 valid_events.append(event_path)

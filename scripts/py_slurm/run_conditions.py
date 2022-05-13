649727c4892e6aa765363d96e31f5acf2b9d760f
hiro <hirofeynman@gmail.com>
2022-05-13 09:59:06 +0200

    preprocess

--- scripts/py_slurm/run_conditions.py
+++ scripts/py_slurm/run_conditions.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     param_dir = Path(sys.argv[3])
 
     # Get the parameters
-    params = read_json(param_dir, "conditions-params-nv.json")
+    params = read_json(param_dir, "conditions-params-voice.json")
 
     # Set up directories
     root = Path(params["root"])

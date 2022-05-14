79fb89bc5b3d409c2d5e740c2c59c05a93967c06
hiro <hirofeynman@gmail.com>
2022-05-14 11:18:18 +0200

    slurm

--- scripts/pipeline/standard_pipeline.py
+++ scripts/pipeline/standard_pipeline.py
@@ -6,25 +6,71 @@ from pathlib import Path
 
 from utils.file_access import load_json
 from utils.logger import get_logger
+from utils.slurm_tools import init_status, get_subject_id_list, get_area_id_list
+from scripts.pipeline.downsample_pipeline import submit_downsample
 
-SLEEP = 10
+SLEEP = 10  # in seconds
+
+
+def _init_all_status(params: dict):
+
+    # Downsampling status, per subject
+    slurm = params["downsample"]["slurm"]
+    downsample = init_status(name="downsample", n_tasks=slurm["n-tasks"], mem=slurm["mem"],
+                             id_list=get_subject_id_list())
+
+    # Epoching status, per subject
+    slurm = params["epoching"]["slurm"]
+    epoching = init_status(name="epoching", n_tasks=slurm["n-tasks"], mem=slurm["mem"],
+                           id_list=get_subject_id_list())
+
+    # Source localization, per subject
+    slurm = params["source-localization"]["slurm"]
+    source = init_status(name="source-localization", n_tasks=slurm["n-tasks"], mem=slurm["mem"],
+                         id_list=get_subject_id_list())
+
+    # Dataset generation, per cortical area
+    slurm = params["dataset"]["slurm"]
+    dataset = init_status(name="dataset", n_tasks=slurm["n-tasks"], mem=slurm["mem"],
+                          id_list=get_area_id_list())
+
+    # Condition conversion, per cortical area
+    slurm = params["conversion"]["slurm"]
+    condition = init_status(name="condition", n_tasks=slurm["n-tasks"], mem=slurm["mem"],
+                            id_list=get_area_id_list())
+
+    # Classification, per cortical area
+    slurm = params["classification"]["slurm"]
+    classification = init_status(name="classification", n_tasks=slurm["n-tasks"], mem=slurm["mem"],
+                                 id_list=get_area_id_list())
+
+    return {"downsample": downsample, "epoching": epoching, "source-localization": source,
+            "dataset": dataset, "condition": condition, "classification": classification}
 
 
 def main(param_path):
+
     # Read the parameters
     params = load_json(param_path)
     logger = get_logger(params["log-dir"], params["log-name"])
 
+    # Print initial status
     start = datetime.now()
     logger.info(f"Launching standard pipeline at {start}")
     logger.info(f"Following parameters are used ----------------------------------------------------------------------")
     logger.info(json.dumps(params, sort_keys=True, indent=4))
 
-    downsample, epoch, source, dataset, condition, analysis = False
+    # Initialization
+    root_status = init_status(params)
 
-    while not all(downsample, epoch, source, dataset, condition, analysis):
+    # Periodically check and submit jobs if possible until all jobs are completed (or failed)
+    while not all(root_status["downsample"], root_status["epoching"], root_status["source-localization"],
+                  root_status["dataset"], root_status["condition"], root_status["classification"]):
 
         # Downsample
+        submit_downsample(status_dir=params["status-dir"], script_dir=params["script-dir"], job_dir=params["job-dir"],
+                          log_dir=params["log-dir"], status=root_status["downsample"])
+
         # downsample = submit_downsample(params["downsample"], status)
 
         # Epoch
@@ -39,15 +85,17 @@ def main(param_path):
         # Condition
         # condition = submit_condition(params["condition"], status)
 
-        # Analysis
+        # Classification
         # analysis = submit_analysis(params["analysis"], status)
 
         sleep(SLEEP)
 
+    # Print the resulting results
     end = datetime.now()
-    logger.info(f"Process terminated at {end}")  # todo success
+    logger.info(f"Process terminated at {end}")
     logger.info(f"Process took {end - start}")
-    print(f"")
+    logger.info(f"Process final status -------------------------------------------------------------------------------")
+    logger.info(json.dumps(root_status, sort_keys=True, indent=4))
 
 
 if __name__ == "__main__":
@@ -55,6 +103,3 @@ if __name__ == "__main__":
     param_path = Path(sys.argv[1])
 
     main(param_path)
-
-
-

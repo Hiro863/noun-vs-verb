87df553c4f4754db7fe0007768066cfe05ff0dc6
hiro <hirofeynman@gmail.com>
2022-05-15 23:09:24 +0200

    downsample-slurm

--- utils/slurm_tools.py
+++ utils/slurm_tools.py
@@ -1,7 +1,14 @@
+import logging
 import os
 import pickle
 from pathlib import Path
 from datetime import datetime
+
+from utils.logger import get_logger
+
+logger = get_logger(file_name="slurm-tools")
+logger.setLevel(logging.INFO)
+
 
 # These are not valid subject numbers
 not_subject = [14, 15, 18, 21, 23, 41, 43, 47, 51, 56, 60, 67, 82, 91, 112]
@@ -55,12 +62,17 @@ def save_status(status, path: Path):
 
 
 def make_job_file(script: str, script_dir: Path, log_dir: Path, job_dir: Path,
-                  name: str, array_str: str, output: str, params: list,
+                  name: str, array_str: str, output: str, params: dict,
                   n_tasks_per_node=1, mem_per_cpu=1):
 
+    logger.info(f"Making a job file for the script {script}")
+
+    # Make sure directories exist
     logs_dir = log_dir / name
-    if not logs_dir.exists:
+    if not logs_dir.exists():
         os.makedirs(logs_dir)
+    if not job_dir.exists():
+        os.makedirs(job_dir)
 
     job_file = ""
 
@@ -78,13 +90,19 @@ def make_job_file(script: str, script_dir: Path, log_dir: Path, job_dir: Path,
     job_file += f"export PYTHONPATH=$PYTHONPATH:/data/home/hiroyoshi/scripts/meg-mvpa\n"
     job_file += f"python $scripts_dir/{script}"
 
-    for param in params:
-        job_file += f"{param} "
+    for name, value in params.items():
+
+        if isinstance(value, list):
+            job_file += f"--{name} {[str(item) + ' ' for item in value]}"
+        else:
+            job_file += f"--{name} {value}"
     job_file += "\n"
 
     job_path = job_dir / f"{name}.job"
 
     with open(job_path, "w") as f:
         f.write(job_file)
+
+    logger.info(f"The job file was added to the job directory \n {job_file}")
 
     return job_path

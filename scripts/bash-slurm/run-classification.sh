e12b8ce7b962066573754af59d51af5146468106
hiro <hirofeynman@gmail.com>
2022-05-17 15:41:09 +0200

    stuff

--- scripts/bash-slurm/run-classification.sh
+++ scripts/bash-slurm/run-classification.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=classification
-#SBATCH --array=0,10,14,16,30,36,38,40,58,60,62,64   ###0-68  ## parcel ID
-#SBATCH --ntasks-per-node=20   ## number of cores per parcel
+#SBATCH --array=0,14,30,40,64   ##0,10,14,16,30,36,38,40,58,60,62,64   ###0-68  ## parcel ID
+#SBATCH --ntasks-per-node=32   ## number of cores per parcel
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=hiroyoshi.yamasaki@etu.univ-amu.fr
 #SBATCH --chdir=/data/home/hiroyoshi/logs/classification

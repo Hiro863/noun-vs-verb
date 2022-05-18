c2c165baeb56c477a2537d413145a98a0b815da2
hiro <hirofeynman@gmail.com>
2022-05-18 18:27:52 +0200

    stuff

--- scripts/bash-slurm/run-classification.sh
+++ scripts/bash-slurm/run-classification.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --job-name=classification
-#SBATCH --array=0,16,24,28,30,36,38,40,64,66  ##0,10,14,16,30,36,38,40,58,60,62,64   ###0-68  ## parcel ID
-#SBATCH --ntasks-per-node=25   ## number of cores per parcel
+#SBATCH --array=0,14,16,22,24,28,30,36,38,40,60,62,64,66   ##0,10,14,16,30,36,38,40,58,60,62,64   ###0-68  ## parcel ID
+#SBATCH --ntasks-per-node=20   ## number of cores per parcel
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=hiroyoshi.yamasaki@etu.univ-amu.fr
 #SBATCH --chdir=/data/home/hiroyoshi/logs/classification
-#SBATCH --mem-per-cpu=8gb
+#SBATCH --mem-per-cpu=15gb
 
 scripts_dir=/data/home/hiroyoshi/scripts/meg-mvpa/scripts/py_slurm  # scripts directory
 param_dir=/data/home/hiroyoshi/semi-final/param-dir                    # parameter directory

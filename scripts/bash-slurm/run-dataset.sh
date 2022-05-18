1674c21216d6a405db64480003f5a329dca2871e
hiro <hirofeynman@gmail.com>
2022-05-18 13:47:40 +0200

    stuff

--- scripts/bash-slurm/run-dataset.sh
+++ scripts/bash-slurm/run-dataset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=dataset
-#SBATCH --array=0,10,14,16,30,36,38,40,58,60,62,64       ###0-68  ## -68     ##1-447          ## cortical area IDs, for aparc_sub
+#SBATCH --array=0,14,16,22,24,28,30,36,38,40,60,62,64,66       ###0-68  ## -68  ##1-447cortical area IDs, for aparc_sub
 #SBATCH --ntasks-per-node=1   ## number of cores per subject
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=hiroyoshi.yamasaki@etu.univ-amu.fr
@@ -9,7 +9,7 @@
 #SBATCH --mem-per-cpu=15gb
 
 scripts_dir=/data/home/hiroyoshi/scripts/meg-mvpa/scripts/py_slurm  # scripts directory
-param_dir=/data/home/hiroyoshi/mous_wd/param-dir                    # parameter directory
+param_dir=/data/home/hiroyoshi/semi-final/param-dir                    # parameter directory
 
 
 export PYTHONPATH=$PYTHONPATH:/data/home/hiroyoshi/scripts/meg-mvpa
